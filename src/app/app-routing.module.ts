5e92821392514f5d111c7127c81eef74c841f307
Felipe Tadeu <felipetadeumaiachagas@gmail.com>
2019-11-28 21:48:28 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroPacienteComponent } from './_components/cadastro-paciente/cadastro-paciente.component';
+import { LayoutComponent } from './_components/_utils/layout/layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cadastro-paciente',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: CadastroPacienteComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'cadastro-paciente',
+    pathMatch: 'prefix'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroPacienteComponent } from './_components/cadastro-paciente/cadastro-paciente.component';
+import { LayoutComponent } from './_components/_utils/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroPacienteComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
